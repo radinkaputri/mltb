cc02329f1f3298a6fe323158ae721616439123b8
clyfly <deni@xyradcloud.my.id>
2024-10-08 15:30:51 +0700

    rebase 2/2
    Signed-off-by: clyfly <deni@xyradcloud.my.id>

--- update.py
+++ update.py
@@ -1,47 +1,69 @@
-from dotenv import load_dotenv, dotenv_values
+import sys
+from os import path, environ
 from logging import (
+    INFO,
+    ERROR,
+    Formatter,
     FileHandler,
     StreamHandler,
-    INFO,
-    basicConfig,
-    error as log_error,
-    info as log_info,
+    info,
+    error,
     getLogger,
-    ERROR,
+    basicConfig,
 )
-from os import path, environ, remove
-from pymongo.mongo_client import MongoClient
-from pymongo.server_api import ServerApi
-from subprocess import run as srun
+from subprocess import run
+
+from dotenv import load_dotenv
+from pymongo import MongoClient
+from requests import get
 
 getLogger("pymongo").setLevel(ERROR)
+getLogger("httpx").setLevel(ERROR)
 
 if path.exists("log.txt"):
     with open("log.txt", "r+") as f:
         f.truncate(0)
 
-if path.exists("rlog.txt"):
-    remove("rlog.txt")
 
-basicConfig(
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-    handlers=[FileHandler("log.txt"), StreamHandler()],
-    level=INFO,
+class CustomFormatter(Formatter):
+    def format(self, record):
+        return super().format(record).replace(record.levelname, record.levelname[:1])
+
+
+formatter = CustomFormatter(
+    "[%(asctime)s] [%(levelname)s] - %(message)s", datefmt="%d-%b-%y %I:%M:%S %p"
 )
+
+file_handler = FileHandler("log.txt")
+file_handler.setFormatter(formatter)
+
+stream_handler = StreamHandler()
+stream_handler.setFormatter(formatter)
+
+basicConfig(handlers=[file_handler, stream_handler], level=INFO)
+
+CONFIG_FILE_URL = environ.get("CONFIG_FILE_URL")
+try:
+    if len(CONFIG_FILE_URL) == 0:
+        raise TypeError
+    try:
+        res = get(CONFIG_FILE_URL)
+        if res.status_code == 200:
+            with open("config.env", "wb+") as f:
+                f.write(res.content)
+        else:
+            error(f"Failed to download config.env {res.status_code}")
+    except Exception as e:
+        error(f"CONFIG_FILE_URL: {e}")
+except Exception:
+    pass
 
 load_dotenv("config.env", override=True)
 
-try:
-    if bool(environ.get("_____REMOVE_THIS_LINE_____")):
-        log_error("The README.md file there to be read! Exiting now!")
-        exit(1)
-except:
-    pass
-
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
 if len(BOT_TOKEN) == 0:
-    log_error("BOT_TOKEN variable is missing! Exiting now")
-    exit(1)
+    error("BOT_TOKEN variable is missing! Exiting now")
+    sys.exit(1)
 
 bot_id = BOT_TOKEN.split(":", 1)[0]
 
@@ -49,42 +71,43 @@ DATABASE_URL = environ.get("DATABASE_URL", "")
 if len(DATABASE_URL) == 0:
     DATABASE_URL = None
 
-if DATABASE_URL is not None:
-    try:
-        conn = MongoClient(DATABASE_URL, server_api=ServerApi("1"))
-        db = conn.mltb
-        old_config = db.settings.deployConfig.find_one({"_id": bot_id})
-        config_dict = db.settings.config.find_one({"_id": bot_id})
-        if old_config is not None:
-            del old_config["_id"]
-        conn.close()
-    except Exception as e:
-        log_error(f"Database ERROR: {e}")
+if DATABASE_URL:
+    conn = MongoClient(DATABASE_URL)
+    db = conn.luna
+    if config_dict := db.settings.config.find_one({"_id": bot_id}):
+        environ["UPSTREAM_REPO"] = config_dict["UPSTREAM_REPO"]
+        environ["UPSTREAM_BRANCH"] = config_dict["UPSTREAM_BRANCH"]
+    conn.close()
 
-UPSTREAM_REPO = "https://github.com/SN-Abdullah-Al-Noman/MirroGram"
-UPSTREAM_BRANCH = "main"
+UPSTREAM_REPO = environ.get("UPSTREAM_REPO", "")
+if len(UPSTREAM_REPO) == 0:
+    UPSTREAM_REPO = "https://github.com/radinkaputri/mltb"
 
-if UPSTREAM_REPO:
-    if path.exists(".git"):
-        srun(["rm", "-rf", ".git"])
+UPSTREAM_BRANCH = environ.get("UPSTREAM_BRANCH", "")
+if len(UPSTREAM_BRANCH) == 0:
+    UPSTREAM_BRANCH = "main"
 
-    update = srun(
-        [
-            f"git init -q \
-                     && git config --global user.email e.anastayyar@gmail.com \
-                     && git config --global user.name mltb \
-                     && git add . \
-                     && git commit -sm update -q \
-                     && git remote add origin {UPSTREAM_REPO} \
-                     && git fetch origin -q \
-                     && git reset --hard origin/{UPSTREAM_BRANCH} -q"
-        ],
-        shell=True,
+if path.exists(".git"):
+    run(["rm", "-rf", ".git"], check=False)
+
+update = run(
+    [
+        f"git init -q \
+                 && git config --global user.email deni@xyradcloud.my.id \
+                 && git config --global user.name clyfly \
+                 && git add . \
+                 && git commit -sm update -q \
+                 && git remote add origin {UPSTREAM_REPO} \
+                 && git fetch origin -q \
+                 && git reset --hard origin/{UPSTREAM_BRANCH} -q"
+    ],
+    shell=True,
+    check=False,
+)
+
+if update.returncode == 0:
+    info("Successfully updated with latest commit from UPSTREAM_REPO")
+else:
+    error(
+        "Something went wrong while updating, check UPSTREAM_REPO if valid or not!"
     )
-
-    if update.returncode == 0:
-        log_info("Successfully updated with latest commit from UPSTREAM_REPO")
-    else:
-        log_error(
-            "Something went wrong while updating, check UPSTREAM_REPO if valid or not!"
-        )
